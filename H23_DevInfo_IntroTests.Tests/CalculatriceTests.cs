a9713f7f355e74f486b86660c4305f56e567eeeb
Tidjani Belmansour <tbelmansour@cofomo.com>
2023-05-03 15:03:58 -0400

    Add project files.

---
+++ H23_DevInfo_IntroTests.Tests/CalculatriceTests.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace H23_DevInfo_IntroTests.Tests
+{
+    [TestClass]
+    public class CalculatriceTests
+    {
+
+        private Calculatrice calc;
+
+
+        [TestInitialize]
+        public void Initialiser()
+        {
+            calc = new Calculatrice();
+        }
+
+
+        [TestMethod]
+        [TestCategory("Méthodes d'addition")]
+        public void Addition_UnPlusDeux_RetourneTrois()
+        {
+            // Arrange
+            int a = 1;
+            int b = 2;
+            int attendue = 3;
+            
+            // Act
+            int obtenue = calc.Addition(a, b);
+
+            // Assert
+            Assert.AreEqual(attendue, obtenue);
+        }
+
+
+        [TestMethod]
+        [Timeout(300)]
+        [TestCategory("Méthodes d'addition")]
+        public void Addition_ZeroPlusZero_RetourneZero()
+        {
+            // Arrange
+            int a = 0;
+            int b = 0;
+            int attendue = 0;
+
+            // Act
+            int obtenue = calc.Addition(a, b);
+
+            // Assert
+            Assert.AreEqual(attendue, obtenue);
+        }
+
+
+        [TestMethod]
+        [TestCategory("Méthodes de soustraction")]
+        public void Soustraction_UnMoinsDeux_RetourneMoinsUn()
+        {
+            // Arrange
+            int a = 1;
+            int b = 2;
+            int attendue = -1;
+
+            // Act
+            int obtenue = calc.Soustraction(a, b);
+
+            // Assert
+            Assert.AreEqual(attendue, obtenue);
+            Assert.IsTrue(obtenue < 0);
+        }
+
+
+        [TestMethod]
+        //[ExpectedException(typeof(DivideByZeroException))]
+        public void Division_1Par0_RetourneDivideByZeroException()
+        {
+            // Arrange
+            int a = 2;
+            int b = 0;
+
+
+            // Act
+            // calc.Division(a, b);
+
+
+            // Act & Assert
+            Assert.ThrowsException<DivideByZeroException>(() => calc.Division(a, b));
+        }
+
+
+        [TestMethod]
+        public void PuissanceDeux_ANegatif_RetourneArgumentOutOfRangeException()
+        {
+            // Arrange
+            int a = -1;
+            string parametreExceptionAttendue = "a";
+            string messsageExceptionAttendue = "a doit être positif ou nul (Parameter 'a')";
+
+
+            // Act & Assert
+            var exceptionObtenue = Assert.ThrowsException<ArgumentOutOfRangeException>(() => calc.PuissanceDeux(a));
+            Assert.AreEqual(parametreExceptionAttendue, exceptionObtenue.ParamName);
+            Assert.AreEqual(messsageExceptionAttendue, exceptionObtenue.Message);
+        }
+    }
+}
---
+++ H23_DevInfo_IntroTests/Calculatrice.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H23_DevInfo_IntroTests
+{
+    public class Calculatrice
+    {
+        // 1 + 2 = 3
+        // 0 + 0 = 0
+        // 1 + 0 = 1
+        // -1 + 2 = 1
+        public int Addition(int a, int b)  
+        {
+            return a + b;
+        }
+
+        // 1 - 2 = -1
+        // 0 - 0 = 0
+        // 1 - 0 = 1
+        // -1 - 2 = -3
+        public int Soustraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        // 1 * 2 = 2
+        // 0 * 0 = 0
+        // 1 * 0 = 0
+        // -1 * 2 = -2
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        // 1 / 2 = 0
+        // 0 / 0 = exception
+        // 1 / 0 = exception
+        // -1 / 2 = 0
+        // 2 / 2 = 1
+        // 3 / 2 = 1
+        // 6 / 2 = 3
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+
+
+
+        public int PuissanceDeux(int a)
+        {
+            if(a < 0)
+            {
+                throw new ArgumentOutOfRangeException("a", "a doit être positif ou nul");
+            }
+            else
+                return a * a;
+        }
+    }
+}
